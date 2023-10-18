61f7bdc9471def1d300ebe9c9e52efd3520a60a2
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-10-18 01:49:51 -0300

    feat: Creación seeding

---
+++ KriptonApi/DataAccess/DataBaseSeeding/IEntitySeeder.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KriptonApi.DataAccess.DataBaseSeeding
+{
+    public interface IEntitySeeder
+    {
+        void SeedDataBase(ModelBuilder modelBuilder);
+    }
+}
