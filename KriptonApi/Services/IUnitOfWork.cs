f77f95d6b63d53feea91ee19658367477cd963e2
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-10-18 01:51:02 -0300

    feat: Creación Services

---
+++ KriptonApi/Services/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using KriptonApi.DataAccess.Repositories;
+
+namespace KriptonApi.Services
+{
+    public interface IUnitOfWork
+    {
+        public UsuarioRepository UsuarioRepository { get; }
+        Task<int> complete();
+    }
+}
