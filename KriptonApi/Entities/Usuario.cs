ef35c2cfc3ab64674950c93908732603f55f91c4
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-10-18 01:48:55 -0300

    feat: Creación Entities y Dtos

---
+++ KriptonApi/Entities/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace KriptonApi.Entities
+{
+    public class Usuario
+    {
+        [Key]
+        [Column("IdUsuario", TypeName = "int")]
+        public int IdUsuario { get; set; }
+        [Required]
+        [Column("Nombre", TypeName = "VARCHAR (100)")]
+        public string Nombre { get; set; }
+        [Required]
+        [Column("Apellido", TypeName = "VARCHAR (100)")]
+        public string Apellido { get; set; }
+        [Required]
+        [Column("Dni", TypeName = "int")]
+        public int Dni { get; set; }
+        [Required]
+        [Column("Email", TypeName = "VARCHAR (100)")]
+        public string Email { get; set; }
+        [Required]
+        [Column("CLave", TypeName = "VARCHAR (100)")]
+        public string Clave { get; set; }
+        [Required]
+        [Column("Activo", TypeName = "bit")]
+        public bool Activo { get; set; }
+    }
+}
