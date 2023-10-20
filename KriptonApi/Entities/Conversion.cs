cf71812afb4f7b00c758cab477e7ee49eee4d22a
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-10-20 02:07:28 -0300

    Creacion de entidad Conversion

---
+++ KriptonApi/Entities/Conversion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace KriptonApi.Entities
+{
+    public class Conversion
+    {
+        [Key]
+        [Column("IdConversion", TypeName = "int")]
+        public int IdConversion { get; set; }
+        [Required]
+        [ForeignKey("Cuentas")]
+        public int IdCuentaOrigen { get; set; }
+        [Required]
+        [ForeignKey("Cuentas")]
+        public int IdCuentaDestino { get; set; }
+        [Required]
+        [Column("MontoConvertido", TypeName = "decimal")]
+        public double MontoConvertido { get; set; }
+        [Required]
+        [Column("FechaConversion", TypeName = "Date")]
+        public DateTime FechaConversion { get; set; }
+        [Required]
+        [ForeignKey("TipoConversion")]
+        public int IdTipoConversion { get; set; }
+
+    }
+}
---
+++ KriptonApi/Entities/TipoConversion.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace KriptonApi.Entities
+{
+    public class TipoConversion
+    {
+        [Key]
+        [Column("IdTipoConversion", TypeName = "int")]
+        public int IdTipoConversion { get; set; }
+        [Required]
+        [Column("Descr", TypeName = "VARCHAR (100)")]
+        public string Descr { get; set; }
+
+    }
+}
