185a30e72930b15f1269e6c4e1d7364096b7beb6
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-10-20 02:07:06 -0300

    Creacion de entidad transacciones

---
+++ KriptonApi/Entities/Transaccion.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KriptonApi.Entities
+{
+    public class Transaccion
+    {
+        [Key]
+        [Column ("IdTransacciones", TypeName = "int") ]
+        public int IdTransacciones { get; set; }
+        [Required]
+        [ForeignKey("Cuentas")]
+        public int IdCuentaOrigen { get; set; }
+        [Required]
+        [ForeignKey("Cuentas")]
+        public int IdCuentaDestino { get; set; }
+        [Required]
+        [ForeignKey("TipoTransaccion")]
+        public int IdTipoTransaccion { get; set; }
+        [Required]
+        [Column("Monto", TypeName = "decimal")]
+        public double Monto { get; set; }
+        [Required]
+        [Column("FechaTransaccion", TypeName = "Date")]
+        public DateTime FechaTransaccion { get; set; }
+
+
+    }
+}
