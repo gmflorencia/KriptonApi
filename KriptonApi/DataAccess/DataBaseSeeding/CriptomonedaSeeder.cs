3ad9472c3d4aa88a3b970c7ce2b77b2171f13496
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-10-20 02:08:14 -0300

    creacion de seeders

---
+++ KriptonApi/DataAccess/DataBaseSeeding/CriptomonedaSeeder.cs
@@ -0,0 +1,35 @@
+﻿using KriptonApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KriptonApi.DataAccess.DataBaseSeeding
+{
+    public class CriptomonedaSeeder : IEntitySeeder
+    {
+        public void SeedDataBase(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CriptomonedaSeeder>().HasData(
+                new Criptomoneda
+                {
+                    IdCripto = 1,
+                    Nombre = "BTC",
+                    TasaConversion = 0.25,
+
+                },
+                new Criptomoneda
+                {
+                    IdCripto = 2,
+                    Nombre = "ETH",
+                    TasaConversion = 0.35
+                },
+                new Criptomoneda
+                {
+                    IdCripto = 3,
+                    Nombre = "LTC",
+                    TasaConversion = 0.30
+
+                }
+                );
+
+        }
+    }
+}
---
+++ KriptonApi/DataAccess/DataBaseSeeding/CuentaSeeder.cs
@@ -0,0 +1,52 @@
+﻿using KriptonApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace KriptonApi.DataAccess.DataBaseSeeding
+{
+    public class CuentaSeeder : IEntitySeeder
+    {
+        public void SeedDataBase(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CuentaSeeder>().HasData(
+                new Cuenta
+                {
+                    IdCuenta = 1,
+                    IdTipoCuenta = 1,
+                    Saldo = 50000,
+                    Cbu = "01702046600000087865",
+                    Alias = "Patria.gorro.Caucho",
+                    NumeroCuenta = 204878658,
+                    UUID = "12345678123456781234567812345678",
+                    Activo = true,
+
+
+                },
+                new Cuenta
+                {
+                    IdCuenta = 2,
+                    IdTipoCuenta = 2,
+                    Saldo = 80000,
+                    Cbu = "0290000100000000058382",
+                    Alias = "Raiz.Arbol.Planta",
+                    NumeroCuenta = 5838,
+                    UUID = "123456781234-56781234567812345678",
+                    Activo = true,
+                },
+                new Cuenta
+                {
+                    IdCuenta = 3,
+                    IdTipoCuenta = 3,
+                    Saldo = 90000,
+                    Cbu = "0110599520000001235579",
+                    Alias = "Alfil.Oslo.Rima",
+                    NumeroCuenta = 123557,
+                    UUID = "123456781234-56781234567812785643",
+                    Activo = true,
+
+                }
+                );
+                
+        }
+    }
+}
