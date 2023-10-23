0874d3b659108ade1a754a034ca5074516c6a1b3
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-10-23 12:51:13 -0300

    feat: Creación de DataBaseSeeding Transaccion

---
+++ KriptonApi/DataAccess/DataBaseSeeding/TransaccionSeeder.cs
@@ -0,0 +1,45 @@
+﻿using KriptonApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KriptonApi.DataAccess.DataBaseSeeding
+{
+    public class TransaccionSeeder : IEntitySeeder
+    {
+        public void SeedDataBase(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Transaccion>().HasData(
+                new Transaccion
+                {
+                    IdTransaccion = 1,
+                    IdCuentaOrigen = 1,
+                    IdCuentaDestino = 2,
+                    IdTipoTransaccion = 3,
+                    Monto = 10000,
+                    FechaTransaccion = DateTime.Now
+
+                },
+                new Transaccion
+                {
+                    IdTransaccion = 2,
+                    IdCuentaOrigen = 2,
+                    IdCuentaDestino = 3,
+                    IdTipoTransaccion = 1,
+                    Monto = 20000,
+                    FechaTransaccion = DateTime.Now
+
+                },
+                new Transaccion
+                {
+                    IdTransaccion = 3,
+                    IdCuentaOrigen = 3,
+                    IdCuentaDestino = 1,
+                    IdTipoTransaccion = 2,
+                    Monto = 30000,
+                    FechaTransaccion = DateTime.Now
+
+                }
+                );
+
+        }
+    }
+}
