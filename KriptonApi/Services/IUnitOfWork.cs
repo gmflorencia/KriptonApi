5dc5bcff511d3430a6470f7680b13e7e691b1eff
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-10-23 12:46:13 -0300

    chore: Actualización Capa Services

--- KriptonApi/Services/IUnitOfWork.cs
+++ KriptonApi/Services/IUnitOfWork.cs
@@ -5,6 +5,9 @@ namespace KriptonApi.Services
     public interface IUnitOfWork
     {
         public UsuarioRepository UsuarioRepository { get; }
+        public CuentaRepository CuentaRepository { get; set; }
+        public CriptomonedaRepository CriptomonedaRepository { get; set; }
+        public TransaccionRepository TransaccionRepository { get; set; }
         Task<int> complete();
     }
 }
--- KriptonApi/Services/UnitOfWorkServices.cs
+++ KriptonApi/Services/UnitOfWorkServices.cs
@@ -8,19 +8,20 @@ namespace KriptonApi.Services
     {
         private readonly AppDbContext _context;
         public UsuarioRepository UsuarioRepository { get; set; }
+        public CriptomonedaRepository CriptomonedaRepository { get; set; }
+        public CuentaRepository CuentaRepository { get; set; }
+        public TransaccionRepository TransaccionRepository { get; set; }
         public UnitOfWorkServices(AppDbContext context)
         {
             _context = context;
             UsuarioRepository = new UsuarioRepository(_context);
+            CriptomonedaRepository = new CriptomonedaRepository(_context);
+            CuentaRepository = new CuentaRepository(_context);
+            TransaccionRepository = new TransaccionRepository(_context);
         }
-        public Task<int> Complete()
-        {
-            return _context.SaveChangesAsync();
-        }
-
         public Task<int> complete()
         {
-            throw new NotImplementedException();
+            return _context.SaveChangesAsync();
         }
     }
 }
