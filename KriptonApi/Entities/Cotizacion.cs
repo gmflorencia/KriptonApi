8cbee6aa457d6a83d5200837409df968e81ba2e9
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-10-23 12:47:35 -0300

    feat: Creación de entities Cotizacion y Moneda

---
+++ KriptonApi/Entities/Cotizacion.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using KriptonApi.DTOs;
+
+namespace KriptonApi.Entities
+{
+    public class Cotizacion
+    {
+        [Key]
+        [Column("IdCotizacion", TypeName = "int")]
+        public int IdCotizacion { get; set; }
+
+        [Required]
+        [Column("IdMoneda", TypeName = "int")]
+        public int IdMoneda { get; set; }
+        [ForeignKey("IdMoneda")]
+        public Moneda Moneda { get; set; }
+
+        [Required]
+        [Column("Compra", TypeName = "money")]
+        public decimal Compra { get; set; }
+
+        [Required]
+        [Column("Venta", TypeName = "money")]
+        public decimal Venta { get; set; }
+
+        [Required]
+        [Column("Activo", TypeName = "bit")]
+        public bool Activo { get; set; }
+    }
+}
---
+++ KriptonApi/Entities/Moneda.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using KriptonApi.DTOs;
+
+namespace KriptonApi.Entities
+{
+    public class Moneda
+    {
+        [Key]
+        [Column("IdMoneda", TypeName = "INT")]
+        public int IdMoneda { get; set; }
+        
+        [Required]
+        [Column("Descripcion", TypeName = "VARCHAR(20)")]
+        public string Descripcion { get; set; }
+
+        [Required]
+        [Column("Simbolo", TypeName = "VARCHAR(5)")]
+        public string Simbolo { get; set; }
+
+        [Required]
+        [Column("Activo", TypeName = "BIT")]
+        public bool Activo { get; set; }
+    }
+}
