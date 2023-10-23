cf8cc33f970713168f09ab42ca58e2497ca0486f
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-10-23 12:50:40 -0300

    feat: Creación de DataBaseSeeding Cotización y Moneda

---
+++ KriptonApi/DataAccess/DataBaseSeeding/CotizacionSeeder.cs
@@ -0,0 +1,29 @@
+﻿using KriptonApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KriptonApi.DataAccess.DataBaseSeeding
+{
+    public class CotizacionSeeder : IEntitySeeder
+    {
+        public void SeedDataBase(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cotizacion>().HasData(
+                new Cotizacion
+                {
+                    IdCotizacion = 1,
+                    IdMoneda = 1,
+                    Compra = 347.50M,
+                    Venta = 365.5M,
+                    Activo =  true,
+                },
+                new Cotizacion
+                {
+                    IdCotizacion = 2,
+                    IdMoneda = 2,
+                    Compra = 369.89M,
+                    Venta = 370.47M,
+                    Activo = true,
+                });
+        }
+    }
+}
---
+++ KriptonApi/DataAccess/DataBaseSeeding/MonedaSeeder.cs
@@ -0,0 +1,28 @@
+﻿using KriptonApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KriptonApi.DataAccess.DataBaseSeeding
+{
+    public class MonedaSeeder : IEntitySeeder
+    {
+        public void SeedDataBase(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Moneda>().HasData(
+                new Moneda
+                {
+                    IdMoneda = 1,
+                    Descripcion = "Dólar",
+                    Simbolo = "u$s",
+                    Activo = true,
+                },
+                new Moneda
+                {
+                    IdMoneda = 2,
+                    Descripcion = "Euro",
+                    Simbolo = "€",
+                    Activo = true,
+                }
+                );
+        }
+    }
+}
