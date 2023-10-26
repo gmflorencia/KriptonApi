6cbfe20a770264b5337c72c66ced43ca76771c2f
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-10-26 03:01:15 -0300

    Creacion de dto para compra de cripto

---
+++ KriptonApi/DTOs/CompraCriptoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace KriptonApi.DTOs
+{
+    public class CompraCriptoDto
+    {
+        public int IdCuentaOrigen { get; set; }
+        public int IdCuentaDestino { get; set; }
+        public int IdTipoTransaccion { get; set; }
+        public int IdCripto { get; set; }
+        public decimal Monto { get; set; }
+        public DateTime FechaTransaccion { get; set; }
+    }
+}
